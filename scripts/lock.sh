c8861d31673a4f2c81660069c3a1b06d2ecbde4c
probe2k <kyash054@gmail.com>
2022-10-07 15:51:02 +0530

    setup slock for xsession-lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-TMPBG=/tmp/screen.png
-LOCK=$HOME/.config/scripts/lock.png
-ffmpeg -f x11grab -i $DISPLAY -i $LOCK -filter_complex "gblur=20,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-i3lock -i $TMPBG
-rm $TMPBG
+#TMPBG=/tmp/screen.png
+#LOCK=$HOME/.config/scripts/lock.png
+#ffmpeg -f x11grab -i $DISPLAY -i $LOCK -filter_complex "gblur=20,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+#i3lock -i $TMPBG
+#rm $TMPBG
+slock
