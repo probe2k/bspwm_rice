eb6038728d460f510f4a457618c684b7b15bc1f8
probe2k <kyash054@gmail.com>
2023-02-07 02:32:17 +0530

    make indent context_aware

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -15,6 +15,8 @@ function M.config()
 
 	indent.setup({
 		space_char_blankline = ' ',
+		show_current_context = true,
+		show_current_context_start = true,
 		char_highlight_list = {
 			"IndentBlanklineIndent1",
 			"IndentBlanklineIndent2",
