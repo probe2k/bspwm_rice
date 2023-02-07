fdaae5fabd025223e8efc58dea572a7597ad87d1
probe2k <kyash054@gmail.com>
2023-02-07 16:58:39 +0530

    add diagnostics symbol

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -22,7 +22,7 @@ function M.config()
 		keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 		keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
 		keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-		keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+		keymap.set("n", "<leader>f", "<cmd>Lspsaga code_action<CR>", opts)
 		keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
 		keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
 		keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
@@ -92,6 +92,12 @@ function M.config()
 		capabilities = capabilities,
 		on_attach = on_attach,
 	})
+
+	local signs = { Error = "ﮊ ", Warn = " ", Hint = " ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
+	end
 end
 
 return M
