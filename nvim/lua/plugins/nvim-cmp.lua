3e8e5b2e43b3f6ff31f64c460c3023a640ee9995
probe2k <kyash054@gmail.com>
2023-02-14 01:09:07 +0530

    re-route keybind for doc scroll

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -30,8 +30,8 @@ function M.config()
 		mapping = cmp.mapping.preset.insert({
 			["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 			["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-b>"] = cmp.mapping.scroll_docs(4),
+			["<C-f>"] = cmp.mapping.scroll_docs(-4),
 			["<C-space>"] = cmp.mapping.complete(), -- show completion suggestions
 			["<C-e>"] = cmp.mapping.abort(), -- close completion window
 			["<CR>"] = cmp.mapping.confirm({ select = false }),
