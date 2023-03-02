7db59cb5d40dacb10d4114bf98eb69ab8bc622e6
probe2k <kyash054@gmail.com>
2023-03-02 22:54:05 +0530

    keybinds -> { nvim-tree } : callback

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,13 +6,84 @@ function M.config()
 		return
 	end
 
+	local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+	local list = {
+		{ key = "+", cb = tree_cb("cd") },
+	}
+
 	nvimtree.setup({
 		sort_by = "case_sensitive",
+		disable_netrw = true,
+		hijack_netrw = true,
+		update_cwd = true,
+		update_focused_file = {
+			enable = true,
+			update_cwd = false,
+		},
+		view = {
+			mappings = {
+				list = list
+			},
+			adaptive_size = false,
+			side = "right",
+			width = 30,
+			hide_root_folder = false,
+		},
+		git = {
+			enable = true,
+			ignore = false,
+		},
+		filesystem_watchers = {
+			enable = true,
+		},
+		actions = {
+			open_file = {
+				resize_window = false,
+			},
+		},
 		renderer = {
+			highlight_git = true,
 			group_empty = true,
+			highlight_opened_files = "none",
+
+			indent_markers = {
+				enable = true,
+			},
+
+			icons = {
+				show = {
+					file = true,
+					folder = true,
+					folder_arrow = true,
+					git = true,
+				},
+				glyphs = {
+					default = "",
+					symlink = "",
+					folder = {
+						default = "",
+						empty = "",
+						empty_open = "",
+						open = "",
+						symlink = "",
+						symlink_open = "",
+						arrow_open = "",
+						arrow_closed = "",
+					},
+					git = {
+						unstaged = "✗",
+						staged = "✓",
+						unmerged = "",
+						renamed = "➜",
+						untracked = "★",
+						deleted = "",
+						ignored = "◌",
+					},
+				},
+			},
 		},
 		filters = {
-			dotfiles = true,
+			dotfiles = false,
 		},
 	})
 end
