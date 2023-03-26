ccc70a8a78d832dd207b84589d1a29b23d8add56
probe2k <kyash054@gmail.com>
2023-03-27 00:21:46 +0530

    override arrow keys toggle on nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -27,7 +27,7 @@ function M.config()
 			end,
 		},
 
-		mapping = cmp.mapping.preset.insert({
+		mapping = cmp.mapping({
 			["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 			["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 			["<C-b>"] = cmp.mapping.scroll_docs(4),
