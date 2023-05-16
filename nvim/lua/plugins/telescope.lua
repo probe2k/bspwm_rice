8b6409e9c4241c3dfc7271a304f2bc61428bbb8d
probe2k <kyash054@gmail.com>
2023-05-17 05:10:47 +0530

    telescope config add

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,13 +13,33 @@ function M.config()
 
 	telescope.setup({
 		defaults = {
+			prompt_prefix = "   ",
+			selection_caret = "󱓞 ",
 			mappings = {
 				i = {
 					["<esc>"] = actions.close,
 					["<C-k>"] = actions.move_selection_previous,
 					["<C-j>"] = actions.move_selection_next,
 				}
-			}
+			},
+			layout_config = {
+				horizontal = {
+					prompt_position = "top",
+					preview_width = 0.55,
+					results_width = 0.8,
+				},
+				vertical = {
+					mirror = false,
+				},
+				width = 0.87,
+				height = 0.80,
+				preview_cutoff = 120,
+			},
+			winblend = 0,
+			border = {},
+			borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+			color_devicons = true,
+			set_env = { ["COLORTERM"] = "truecolor" },
 		}
 	})
 
